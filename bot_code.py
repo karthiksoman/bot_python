4ad8c79b0c3ee14f509b1c5603f9ee0f5196c3dc
karthiksoman <karthi.soman@gmail.com>
2020-02-09 21:35:43 -0800

    added dropdown click

--- bot_code.py
+++ bot_code.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 import sys
+import time
 driver = webdriver.Chrome('/usr/local/bin/chromedriver.exe')
 driver.get("https://www.facebook.com/")
 driver.find_element_by_xpath('//*[@id="email"]').send_keys(sys.argv[1])
 driver.find_element_by_xpath('//*[@id="pass"]').send_keys(sys.argv[2])
 driver.find_element_by_xpath('//*[@id="u_0_2"]').click()
 driver.maximize_window()
+time.sleep(5)
 driver.find_element_by_xpath('//*[@id="userNavigationLabel"]').click()
 # driver.find_element_by_xpath('//*[@id="js_46"]/div/div/ul/li[9]/a/span/span')
 
