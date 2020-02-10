c0aab0878bfbe056e7be6dec3d919815d4ffce32
karthiksoman <karthi.soman@gmail.com>
2020-02-09 21:06:57 -0800

    added lines for entering credentials

--- bot_code.py
+++ bot_code.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+import sys
 driver = webdriver.Chrome('/usr/local/bin/chromedriver.exe')
 driver.get("https://www.facebook.com/")
-driver.close()
+driver.find_element_by_xpath('//*[@id="email"]').send_keys(sys.argv[1])
+driver.find_element_by_xpath('//*[@id="pass"]').send_keys(sys.argv[2])
+driver.find_element_by_xpath('//*[@id="u_0_2"]').click()
+# driver.close()
